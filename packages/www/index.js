3567602dca570a9503febc0c4317f644436cf4d5
leecervan904 <zwlife93@gmail.com>
2024-01-18 10:03:28 +0800

    update www

--- packages/www/index.js
+++ packages/www/index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// danger
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // 允许所有站点访问
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // 允许的请求方法
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // 允许的请求头
+
+  next(); // 继续处理下一个中间件或路由处理程序
+})
+
 const port = 3000;
 
 app.get('/', async (req, res) => {
